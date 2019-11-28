215b24565aa0cf61f2f07cdf923749616dc86a06
Ilia Lesik <ilialesik@gmail.com>
2019-11-28 11:38:43 +0300

    Bump dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
-import { Page, Rectangle, Text } from 'react-figma';
+import {BookletsStyleguide} from "./BookletsStyleguide";
 
 export const App = () => {
     return (
-        <Page name="Page X">
-            <Text characters="Hello, react-figma!"/>
-            <Rectangle style={{ width: 200, height: 100, backgroundColor: '#0ddd25' }} />
-            <Rectangle style={{ width: 200, height: 100, backgroundColor: '#a6dd00' }} />
-        </Page>
+        <>
+            <BookletsStyleguide />
+        </>
     );
 };
